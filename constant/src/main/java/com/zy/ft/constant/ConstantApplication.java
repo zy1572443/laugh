478ce82cd8f8a2dd8b04b39d997f45fe314e83e9
zhangyan <zy1572443@163.com>
2019-07-22 15:41:55 +0800

    add constant and utils moudle

---
+++ constant/src/main/java/com/zy/ft/constant/ConstantApplication.java
@@ -0,0 +1,13 @@
+package com.zy.ft.constant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConstantApplication.class, args);
+  }
+
+}
---
+++ laugh_utils/src/main/java/com/zy/ft/laugh_utils/LaughUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.zy.ft.laugh_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughUtilsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LaughUtilsApplication.class, args);
+  }
+
+}
